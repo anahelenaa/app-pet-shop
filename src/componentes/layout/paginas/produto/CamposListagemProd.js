d9e1f5dea21c37f5658da4abb025dd755ff390c5
ThaisSSam <thais.s.sam@hotmail.com>
2024-12-16 20:37:29 -0300

    Criação da página de produtos

---
+++ src/componentes/layout/paginas/produto/CamposListagemProd.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const CamposListagemProduto = [
+    {
+        nome:'Nome',
+        nomeAtributo: 'nome'
+    },
+    {
+        nome:'Estoque',
+        nomeAtributo: 'estoque'
+    },
+    {
+        nome:'Preço',
+        nomeAtributo: 'preco'
+    },
+    {
+        nome:'Custo',
+        nomeAtributo: 'custo'
+    },
+    {
+        nome:'Fornecedor',
+        nomeAtributo:'nomeFornecedor'
+
+    },
+    {
+        nome:'Excluir',
+        nomeAtributo: 'excluir'
+    },
+    {
+        nome:'Editar',
+        nomeAtributo: 'editar'
+    },
+]
+
+export default CamposListagemProduto
---
+++ src/componentes/layout/paginas/produto/DadosTesteListProd.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+const DadosTesteListProd
+= [
+    {
+        nome: 'Ração Premium',
+        estoque: 50,
+        preco: 120.50,
+        custo: 85.30,
+        nomeFornecedor: 'Pedigree'
+    },
+    
+    {
+        nome: 'Ração Premium',
+        estoque: 50,
+        preco: 120.50,
+        custo: 85.30,
+        nomeFornecedor: 'Pedigree'
+    },
+      
+    {
+        nome: 'Ração Premium',
+        estoque: 50,
+        preco: 120.50,
+        custo: 85.30,
+        nomeFornecedor: 'Pedigree'
+    },
+    
+    {
+        nome: 'Ração Premium',
+        estoque: 50,
+        preco: 120.50,
+        custo: 85.30,
+        nomeFornecedor: 'Pedigree'
+    },
+]
+
+export default DadosTesteListProd
