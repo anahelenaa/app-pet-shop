fda9eb28d17f77584e80aa4aea4220460be64f82
anahelenaa <anahelenaesperanca@gmail.com>
2025-02-05 19:56:01 -0300

    adiciona informações do funcionário logado no header

---
+++ src/componentes/layout/elementos/header/DadosTesteLogin.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const DadosTesteLogin = {
+    "nome":"Ana Helena"
+}
+
+export default DadosTesteLogin
--- src/componentes/layout/elementos/header/Header.js
+++ src/componentes/layout/elementos/header/Header.js
@@ -2,6 +2,7 @@ import {React, useState} from 'react';
 import './header.css';
 import ImagemHeader from '../../../imagens/pata-branca.png';
 import * as MdIcons from 'react-icons/md';
+import DadosTesteLogin from './DadosTesteLogin';
 
 const Header = ({clicarBotao}) => {
 
@@ -18,6 +19,15 @@ const Header = ({clicarBotao}) => {
         <div className='nome-sistema-header'>
             <img src={ImagemHeader} className='imagem-header'/>
             <h1 className='nome-header'>PetShop</h1>
+        </div>
+
+        <div className='dados-login-header'>
+            <a className='link-botao-funcionario-logado-header'>
+                <button className='botao-funcionario-logado-header'>
+                    <i>{<MdIcons.MdPerson/>}</i>
+                </button>
+            </a>
+            <h2>{DadosTesteLogin.nome}</h2>
         </div>
 
         <div className='botoes-header'>
